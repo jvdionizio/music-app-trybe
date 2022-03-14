be795201633c57649f5be964b1e00b1bfe5c4255
jvdionizio <dionizio145@gmail.com>
2022-03-14 11:37:14 -0300

    Pages e Routes Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes a</p>);
+    return (
+      <div>
+        <BrowserRouter>
+          <Login />
+          <Search />
+          <Album />
+          <Favorites />
+          <Profile />
+          <ProfileEdit />
+          <NotFound />
+        </BrowserRouter>
+      </div>);
   }
 }
 
